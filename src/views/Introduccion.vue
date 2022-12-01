b4ce5089119b34723ae3056623e5e35101b7eb78
maronita <maronita@users.noreply.github.com>
2022-12-01 11:06:52 -0500

    VIDEO_INTRO

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -14,7 +14,7 @@
         p.mb-3 Al iniciar este proceso formativo, se hace necesario reconocer algunos elementos importantes desde el uso de conceptos y principales temas a desarrollar, para ello se debe tener en cuenta:
       figure.mb-5(data-aos="fade-right")
         .video
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/X1UbgMwNhxs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/JwhY34xdSmI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       
  
     
